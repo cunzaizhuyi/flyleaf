99eb42e5592b9096d251a2bbdfa66df549f31a73
cunzaizhuyi <877824709@qq.com>
2022-08-27 16:58:29 +0800

    feat:  stepmask story

---
+++ src/stories/StepMask.stories.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import StepMask from '../components/StepMask.svelte';
+</script>
+
+
+<Meta title="StepMask" component={StepMask} />
+
+<Template let:args>
+  <StepMask {...args} />
+</Template>
+
+<Story name="demo">
+  <StepMask></StepMask>
+<!--  <div class="boxes">-->
+<!--    <div id="box1" class="box bg-yellow">A</div>-->
+<!--    <div id="box2" class="box bg-pink">B</div>-->
+<!--    <div id="box3" class="box bg-blue">C</div>-->
+
+<!--  </div>-->
+</Story>
+
+<!--<style lang="scss">-->
+<!--  .boxes{-->
+<!--    .box{-->
+<!--      width: 200px;-->
+<!--      height: 100px;-->
+<!--    }-->
+<!--  }-->
+<!--</style>-->
