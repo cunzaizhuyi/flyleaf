41bfef91d43298b7a405c34d5503f5f65342e2f4
widcardw <lui747353367@qq.com>
2022-08-27 19:30:17 +0800

    feat: add checkbox and switch

---
+++ src/components/CheckBox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let checked = false;
+    export let disabled = false;
+</script>
+
+<label flex items-center class={ disabled ? 'cursor-not-allowed' : 'cursor-pointer'}>
+    <div mr-1 flex item-center>
+        <input type="checkbox" display-none bind:checked="{checked}" disabled="{disabled}" >
+        <div i-ri-checkbox-blank-line icon-btn class={ checked ? 'text-#eb3324' : '' } />
+        <div i-ri-checkbox-fill absolute icon-btn class={ !checked ? 'scale-0' : 'text-#eb3324' } transition-all />
+    </div>
+    <slot />
+</label>
