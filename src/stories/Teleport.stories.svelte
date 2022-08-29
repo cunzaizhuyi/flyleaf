535236d68fab4acbccd26c0e47b2a4039d3eeb3f
yesc <158910214@qq.com>
2022-08-30 01:20:23 +0800

    teleport storybook

---
+++ src/stories/Teleport.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import Teleport from '../components/teleport/Teleport.svelte'
+
+	let disableTeleport = false
+</script>
+
+<Meta title="Components/Teleport" component={Teleport} />
+
+<Story name="demo">
+	<div class="box">
+		<button on:click={() => (disableTeleport = !disableTeleport)}
+			>click and {disableTeleport ? 'use' : 'disabled'} teleport</button
+		>
+		<Teleport disabled={disableTeleport}>
+			teleport is {disableTeleport ? 'not' : ''} in body
+		</Teleport>
+	</div>
+</Story>
