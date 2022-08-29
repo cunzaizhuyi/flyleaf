5439af2958eb6c258560d3a2a914d160c39248ab
yesc <158910214@qq.com>
2022-08-29 23:23:21 +0800

    a simple teleport

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,46 @@
 <script lang="ts">
-  import Counter from '@/components/Counter.svelte';
-  import Marquee from '@/components/Marquee.svelte';
-  import { toast } from '@/components/toast';
-  import StepMask from '@/components/StepMask.svelte';
+	import Counter from '@/components/Counter.svelte'
+	import Marquee from '@/components/Marquee.svelte'
+	import { toast } from '@/components/toast'
+	import StepMask from '@/components/StepMask.svelte'
+	import Teleport from '@/components/teleport/Teleport.svelte'
 
-  const data = ['item1', 'item2', 'item3'];
+	const data = ['item1', 'item2', 'item3']
 
-  // toast('这是toast内容', 1500);
+	// toast('这是toast内容', 1500);
 
-  let isStart = false;
-  setTimeout(() => {
-     isStart = true;
-  }, 1000);
-  const stepArr = [
-    {
-      id: 'card',
-      desc: '第一步：欢迎您来到这个APP'
-    },
-    {
-      id: 'aa',
-      desc: '第二步：'
-    }
-  ];
+	let isStart = false
+	setTimeout(() => {
+		isStart = true
+	}, 1000)
+	const stepArr = [
+		{
+			id: 'card',
+			desc: '第一步：欢迎您来到这个APP',
+		},
+		{
+			id: 'aa',
+			desc: '第二步：',
+		},
+	]
+	let disableTeleport = false
 </script>
 
 <div class="card" id="card">
-  <Counter />
+	<Counter />
 </div>
 <!--<Marquee {data}></Marquee>-->
-<div id="aa" style="width: 400px; margin: 50px"> aa </div>
-<StepMask isStart="{isStart}" stepArr="{stepArr}"></StepMask>
+<div id="aa" style="width: 400px; margin: 50px">aa</div>
+<StepMask {isStart} {stepArr} />
+<button on:click={() => (disableTeleport = !disableTeleport)}
+	>click and {disableTeleport ? 'use' : 'disabled'} teleport</button
+>
+<Teleport disabled={disableTeleport}>
+	teleport is {disableTeleport ? 'not' : ''} in body
+</Teleport>
 
 <style lang="less">
-  .card{
-    background-color: red;
-  }
+	.card {
+		background-color: red;
+	}
 </style>
