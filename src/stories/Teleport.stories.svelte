5bbaddfa592566d5b0ed01180d316cae2ffd5b25
yesc <158910214@qq.com>
2022-08-31 00:09:18 +0800

    fix: 空格符

--- src/stories/Teleport.stories.svelte
+++ src/stories/Teleport.stories.svelte
@@ -1,7 +1,6 @@
 <script>
-	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import { Meta, Story } from '@storybook/addon-svelte-csf'
 	import Teleport from '../components/teleport/Teleport.svelte'
-
 	let disableTeleport = false
 </script>
 
