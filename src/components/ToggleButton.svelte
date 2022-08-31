6632e8758afceeecc26ef8e740ed1506ce14f0ff
cunzaizhuyi <877824709@qq.com>
2022-08-31 23:51:38 +0800

    feat: refactor togglebutton and add doc for it

--- src/components/ToggleButton.svelte
+++ src/components/ToggleButton.svelte
@@ -1,9 +1,22 @@
-<script>
-  export let imgChecked="https://tse4-mm.cn.bing.net/th/id/OIP-C.cGGNewOo82RNOdjHnyHcMAAAAA?pid=ImgDet&rs=1";
-  export let imgUnchecked="https://ts1.cn.mm.bing.net/th/id/R-C.cf59580e66343165cb761a1ab2aaf7e8?rik=E39R8u8Zk2mPng&riu=http%3a%2f%2fbpic.588ku.com%2felement_pic%2f18%2f07%2f02%2f56980234137c3b1a4fc26e860afae85a.jpg&ehk=XpqvmAYWaaPOPQnjjMhC7%2f2u7JeGF396wVdvWa3KMp4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1";
-  export let checked=false;
-  export let onChange=(status)=>{console.log(status)};
-  export let type="img"
+<script lang="ts">
+
+  export let imgChecked = "https://tse4-mm.cn.bing.net/th/id/OIP-C.cGGNewOo82RNOdjHnyHcMAAAAA?pid=ImgDet&rs=1";
+  export let imgUnchecked = "https://ts1.cn.mm.bing.net/th/id/R-C.cf59580e66343165cb761a1ab2aaf7e8?rik=E39R8u8Zk2mPng&riu=http%3a%2f%2fbpic.588ku.com%2felement_pic%2f18%2f07%2f02%2f56980234137c3b1a4fc26e860afae85a.jpg&ehk=XpqvmAYWaaPOPQnjjMhC7%2f2u7JeGF396wVdvWa3KMp4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1";
+  export let checked = false;
+
+  export let onChange = (status) => {
+    console.log(status)
+  }
+
+  $: src = checked ? imgChecked : imgUnchecked;
 </script>
 
-<img on:click={()=>{checked^=1;onChange(checked)}} src={checked?imgChecked:imgUnchecked}>
+<img on:click={()=>{ checked = !checked; onChange(checked)}}
+     src={src} class="leaf-toggle-btn">
+
+<style lang="scss">
+  .leaf-toggle-btn{
+    max-width: 100%;
+    width: 100px;
+  }
+</style>
---
+++ src/stories/ToggleButton.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import ToggleButton from '../components/ToggleButton.svelte';
+
+</script>
+
+
+<Meta title="Components/ToggleButton" component={ToggleButton} />
+
+<!--<Template let:args>-->
+<!--  <div class="boxes">-->
+<!--    <div id="box1" class="box bg-yellow">A</div>-->
+<!--    <div id="box2" class="box bg-pink">B</div>-->
+<!--    <div id="box3" class="box bg-blue">C</div>-->
+<!--&lt;!&ndash;    <StepMask {...args} ></StepMask>&ndash;&gt;-->
+<!--    <slot></slot>-->
+<!--  </div>-->
+<!--</Template>-->
+
+<Story name="demo">
+  <div>click the icon! </div>
+  <ToggleButton></ToggleButton>
+</Story>
+
+
+
